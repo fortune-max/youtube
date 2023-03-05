200f39a148dbcecb462b4a13998b638bc8beb561
fortune-max <fortmax120@gmail.com>
2023-03-05 21:56:37 +0100

    add misc data, logo

---
+++ src/videos.js
@@ -0,0 +1,484 @@
+const sampleVideos = [
+    {
+       id: {
+          videoId: "YQHsXMglC9A"
+       },
+       url: "https://www.youtube.com/watch?v=YQHsXMglC9A",
+       title: "Adele - Hello (Official Music Video)",
+       channelName: "Adele",
+       description: "Directed by Xavier Dolan, @XDolan Follow Adele on: Facebook - https://www.facebook.com/Adele Twitter ...",
+       duration_raw: "6:07",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=YQHsXMglC9A",
+          duration: "6:07",
+          publishedAt: "7 years ago",
+          thumbnails: {
+             id: "YQHsXMglC9A",
+             url: "https://i.ytimg.com/vi/YQHsXMglC9A/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCdLcY7Ni9L3AP5ZQ7RDGNaIo6H3g",
+             height: 404,
+             width: 720
+          },
+          title: "Adele - Hello (Official Music Video)",
+          views: "3050179640"
+       },
+       views: "3050179640"
+    },
+    {
+       id: {
+          videoId: "mHONNcZbwDY"
+       },
+       url: "https://www.youtube.com/watch?v=mHONNcZbwDY",
+       title: "Lionel Richie - Hello (Official Music Video)",
+       channelName: "lionelrichie",
+       description: "Music video by Lionel Richie performing  Hello . © 2003 Motown Records, a Division of UMG Recordings, Inc. #LionelRichie ...",
+       duration_raw: "5:27",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=mHONNcZbwDY",
+          duration: "5:27",
+          publishedAt: "2 years ago",
+          thumbnails: {
+             id: "mHONNcZbwDY",
+             url: "https://i.ytimg.com/vi/mHONNcZbwDY/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLClfqZQqqf4y5Ha1FsD2UhgtOIzkQ",
+             height: 404,
+             width: 720
+          },
+          title: "Lionel Richie - Hello (Official Music Video)",
+          views: "65817970"
+       },
+       views: "65817970"
+    },
+    {
+       id: {
+          videoId: "cymbS0t2jsg"
+       },
+       url: "https://www.youtube.com/watch?v=cymbS0t2jsg",
+       title: "🔥👀 LO MANDA MONCADA ‼️FIDIAMOCI! - Milan Hello Calciomercato - Andrea Longoni  [Marlon Gomes]",
+       channelName: "MILAN HELLO - ANDREA LONGONI",
+       description: "Tutte le news e gli approfondimenti sul Milan! Campo, partite, calciomercato, live, esclusive! SE SEI UN CUORE ROSSONERO, ...",
+       duration_raw: "13:53",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=cymbS0t2jsg",
+          duration: "13:53",
+          publishedAt: "5 hours ago",
+          thumbnails: {
+             id: "cymbS0t2jsg",
+             url: "https://i.ytimg.com/vi/cymbS0t2jsg/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBbfJlZ58ciQ9BHUfIGO_zhw5-d7g",
+             height: 404,
+             width: 720
+          },
+          title: "🔥👀 LO MANDA MONCADA ‼️FIDIAMOCI! - Milan Hello Calciomercato - Andrea Longoni  [Marlon Gomes]",
+          views: "8757"
+       },
+       views: "8757"
+    },
+    {
+       id: {
+          videoId: "CBXujnLUIAY"
+       },
+       url: "https://www.youtube.com/watch?v=CBXujnLUIAY",
+       title: "Coldiac - Hello To Goodbye (Official Visualizer Video)",
+       channelName: "COLDIAC",
+       description: "Coldiac -  Hello  To Goodbye (Official Visualizer Video) https://wmid.lnk.to/ColdiacHTG Coldiac -  Hello  To Goodbye lirik: Back to the ...",
+       duration_raw: "4:45",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=CBXujnLUIAY",
+          duration: "4:45",
+          publishedAt: "18 hours ago",
+          thumbnails: {
+             id: "CBXujnLUIAY",
+             url: "https://i.ytimg.com/vi/CBXujnLUIAY/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC3pCImf6IR_5PctiZiEkzY_0QeAw",
+             height: 404,
+             width: 720
+          },
+          title: "Coldiac - Hello To Goodbye (Official Visualizer Video)",
+          views: "2154"
+       },
+       views: "2154"
+    },
+    {
+       id: {
+          videoId: "be12BC5pQLE"
+       },
+       url: "https://www.youtube.com/watch?v=be12BC5pQLE",
+       title: "Adele - Hello (Lyrics)",
+       channelName: "Aqua Lyrics",
+       description: "Adele -  Hello  (Lyrics) Follow Adele : https://myspace.com/adelelondon​ https://www.facebook.com/adele​ ...",
+       duration_raw: "4:57",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=be12BC5pQLE",
+          duration: "4:57",
+          publishedAt: "2 years ago",
+          thumbnails: {
+             id: "be12BC5pQLE",
+             url: "https://i.ytimg.com/vi/be12BC5pQLE/hqdefault.jpg?sqp=-oaymwE9COADEI4CSFryq4qpAy8IARUAAAAAGAElAADIQj0AgKJDeAHwAQH4Af4JgALQBYoCDAgAEAEYZSBlKGUwDw==&rs=AOn4CLABjrvbrFfjq5UtE5FVpJBlGW4miA",
+             height: 270,
+             width: 480
+          },
+          title: "Adele - Hello (Lyrics)",
+          views: "16117519"
+       },
+       views: "16117519"
+    },
+    {
+       id: {
+          videoId: "tVlcKp3bWH8"
+       },
+       url: "https://www.youtube.com/watch?v=tVlcKp3bWH8",
+       title: "Hello! | Super Simple Songs",
+       channelName: "Super Simple Songs - Kids Songs",
+       description: "Start off your lesson with \" Hello !\", a fun and energetic song to talk about how you feel as you greet each other. Listen to Super ...",
+       duration_raw: "1:19",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=tVlcKp3bWH8",
+          duration: "1:19",
+          publishedAt: "7 years ago",
+          thumbnails: {
+             id: "tVlcKp3bWH8",
+             url: "https://i.ytimg.com/vi/tVlcKp3bWH8/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDaLauLkdNEgIG-CacnuQLfMgiWJA",
+             height: 404,
+             width: 720
+          },
+          title: "Hello! | Super Simple Songs",
+          views: "118926367"
+       },
+       views: "118926367"
+    },
+    {
+       id: {
+          videoId: "fazMSCZg-mw"
+       },
+       url: "https://www.youtube.com/watch?v=fazMSCZg-mw",
+       title: "Pop Smoke - Hello (Audio) ft. A Boogie wit da Hoodie",
+       channelName: "POP SMOKE",
+       description: "Music video by Pop Smoke performing  Hello  (Audio). © 2020 Republic Records, a division of UMG Recordings, Inc. & Victor Victor ...",
+       duration_raw: "3:12",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=fazMSCZg-mw",
+          duration: "3:12",
+          publishedAt: "2 years ago",
+          thumbnails: {
+             id: "fazMSCZg-mw",
+             url: "https://i.ytimg.com/vi/fazMSCZg-mw/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLB-67_1uw3KPkOv_e_9a3Y-mhekgQ",
+             height: 404,
+             width: 720
+          },
+          title: "Pop Smoke - Hello (Audio) ft. A Boogie wit da Hoodie",
+          views: "145659564"
+       },
+       views: "145659564"
+    },
+    {
+       id: {
+          videoId: "fN1Cyr0ZK9M"
+       },
+       url: "https://www.youtube.com/watch?v=fN1Cyr0ZK9M",
+       title: "Hello Hello! Can You Clap Your Hands? | Original  Kids Song | Super Simple Songs",
+       channelName: "Super Simple Songs - Kids Songs",
+       description: "Hello Hello  to everyone visiting from Leo Messi's Instagram post   . Use this song to say “ Hello !” to classmates, parents, and ...",
+       duration_raw: "1:32",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=fN1Cyr0ZK9M",
+          duration: "1:32",
+          publishedAt: "7 years ago",
+          thumbnails: {
+             id: "fN1Cyr0ZK9M",
+             url: "https://i.ytimg.com/vi/fN1Cyr0ZK9M/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDoGE5VzH6qolJfpxiHdWKQL3XlYw",
+             height: 404,
+             width: 720
+          },
+          title: "Hello Hello! Can You Clap Your Hands? | Original  Kids Song | Super Simple Songs",
+          views: "80511840"
+       },
+       views: "80511840"
+    },
+    {
+       id: {
+          videoId: "kK42LZqO0wA"
+       },
+       url: "https://www.youtube.com/watch?v=kK42LZqO0wA",
+       title: "Martin Solveig & Dragonette - Hello (Official Short Video Version HD)",
+       channelName: "Kontor.TV",
+       description: "#MartinSolveig #Dragonette # Hello  ▻ Follow Kontor Records Kontor Top Of The Clubs Playlist: ...",
+       duration_raw: "4:13",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=kK42LZqO0wA",
+          duration: "4:13",
+          publishedAt: "12 years ago",
+          thumbnails: {
+             id: "kK42LZqO0wA",
+             url: "https://i.ytimg.com/vi/kK42LZqO0wA/hqdefault.jpg?sqp=-oaymwEjCOADEI4CSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAD7ZYW5mh8BHNzr1bjUvZsSDStLw",
+             height: 270,
+             width: 480
+          },
+          title: "Martin Solveig & Dragonette - Hello (Official Short Video Version HD)",
+          views: "105897062"
+       },
+       views: "105897062"
+    },
+    {
+       id: {
+          videoId: "aPd9exmH17o"
+       },
+       url: "https://www.youtube.com/watch?v=aPd9exmH17o",
+       title: "TREASURE - ‘HELLO’ M/V",
+       channelName: "TREASURE (트레저)",
+       description: "HELLO  I've been all alone 사람들 속에서도 나 혼자 섬인가 봐 아무리 채워봐도 텅 빈 것만 같아 Everywhere I go 난 미친 것처럼 ...",
+       duration_raw: "3:06",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=aPd9exmH17o",
+          duration: "3:06",
+          publishedAt: "4 months ago",
+          thumbnails: {
+             id: "aPd9exmH17o",
+             url: "https://i.ytimg.com/vi/aPd9exmH17o/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA4529YsHX2M_OA5JD1V4q9vPhgeA",
+             height: 404,
+             width: 720
+          },
+          title: "TREASURE - ‘HELLO’ M/V",
+          views: "64645991"
+       },
+       views: "64645991"
+    },
+    {
+       id: {
+          videoId: "Axx9IMdL4jc"
+       },
+       url: "https://www.youtube.com/watch?v=Axx9IMdL4jc",
+       title: "HELLO -  LIONEL RICHIE lyrics (HD)",
+       channelName: "Bluerose Skansen",
+       description: "No copyright infringement is intended. All credits go to the rightful owners. (Used under fair use policy) Should there be any issues ...",
+       duration_raw: "4:07",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=Axx9IMdL4jc",
+          duration: "4:07",
+          publishedAt: "2 years ago",
+          thumbnails: {
+             id: "Axx9IMdL4jc",
+             url: "https://i.ytimg.com/vi/Axx9IMdL4jc/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDfVZZdxxy4QVaZs7xgyn_587s3og",
+             height: 404,
+             width: 720
+          },
+          title: "HELLO -  LIONEL RICHIE lyrics (HD)",
+          views: "3490822"
+       },
+       views: "3490822"
+    },
+    {
+       id: {
+          videoId: "MqalPR2gaWs"
+       },
+       url: "https://www.youtube.com/watch?v=MqalPR2gaWs",
+       title: "Adele - Hello (Live - An Audience With Adele)",
+       channelName: "Adele Access",
+       description: "An Audience with Adele” concert filmed at the London Palladium on November 6, 2021.",
+       duration_raw: "4:57",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=MqalPR2gaWs",
+          duration: "4:57",
+          publishedAt: "2 months ago",
+          thumbnails: {
+             id: "MqalPR2gaWs",
+             url: "https://i.ytimg.com/vi/MqalPR2gaWs/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBGoZ-xghIxDnIM7I9wNgjVbWDHbg",
+             height: 404,
+             width: 720
+          },
+          title: "Adele - Hello (Live - An Audience With Adele)",
+          views: "424478"
+       },
+       views: "424478"
+    },
+    {
+       id: {
+          videoId: "kDkdtNoW9Rw"
+       },
+       url: "https://www.youtube.com/watch?v=kDkdtNoW9Rw",
+       title: "TREASURE - ‘HELLO’ DANCE PRACTICE VIDEO",
+       channelName: "TREASURE (트레저)",
+       description: "TREASURE #트레저 #2ndMINIALBUM #THESECONDSTEP_CHAPTERTWO # HELLO  #DANCE_PRACTICE_VIDEO ...",
+       duration_raw: "3:11",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=kDkdtNoW9Rw",
+          duration: "3:11",
+          publishedAt: "4 months ago",
+          thumbnails: {
+             id: "kDkdtNoW9Rw",
+             url: "https://i.ytimg.com/vi/kDkdtNoW9Rw/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAcz9Vw6YPYka2bNwgYFPqm8OezRg",
+             height: 404,
+             width: 720
+          },
+          title: "TREASURE - ‘HELLO’ DANCE PRACTICE VIDEO",
+          views: "6029699"
+       },
+       views: "6029699"
+    },
+    {
+       id: {
+          videoId: "gghDRJVxFxU"
+       },
+       url: "https://www.youtube.com/watch?v=gghDRJVxFxU",
+       title: "Hello Song for Kids | Greeting Song for Kids | The Singing Walrus",
+       channelName: "The Singing Walrus - English Songs For Kids",
+       description: "The Singing Walrus is excited to present \" Hello  Song\", a fun and engaging song for young learners to start the day in the ...",
+       duration_raw: "2:11",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=gghDRJVxFxU",
+          duration: "2:11",
+          publishedAt: "5 years ago",
+          thumbnails: {
+             id: "gghDRJVxFxU",
+             url: "https://i.ytimg.com/vi/gghDRJVxFxU/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDyVsUqFhmMZIQsRVveV5VnSys84g",
+             height: 404,
+             width: 720
+          },
+          title: "Hello Song for Kids | Greeting Song for Kids | The Singing Walrus",
+          views: "118629592"
+       },
+       views: "118629592"
+    },
+    {
+       id: {
+          videoId: "T1tl66trXTQ"
+       },
+       url: "https://www.youtube.com/watch?v=T1tl66trXTQ",
+       title: "Adele - Hello",
+       channelName: "Adele - Topic",
+       description: "2015 XL Recordings Ltd Released on: 2015-11-20 #Adele #Adele25.",
+       duration_raw: "4:56",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=T1tl66trXTQ",
+          duration: "4:56",
+          publishedAt: "1 year ago",
+          thumbnails: {
+             id: "T1tl66trXTQ",
+             url: "https://i.ytimg.com/vi/T1tl66trXTQ/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDUvg0_lHvWcdPoj05TzltEGFRqJg",
+             height: 404,
+             width: 720
+          },
+          title: "Adele - Hello",
+          views: "482137"
+       },
+       views: "482137"
+    },
+    {
+       id: {
+          videoId: "stRg7XmWWV4"
+       },
+       url: "https://www.youtube.com/watch?v=stRg7XmWWV4",
+       title: "Karol G, Ozuna - Hello (Official Video)",
+       channelName: "KAROL G",
+       description: "Music video by Karol G, Ozuna performing  Hello . (C) 2016 Universal Music Latino http://vevo.ly/Ebz8pW.",
+       duration_raw: "4:36",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=stRg7XmWWV4",
+          duration: "4:36",
+          publishedAt: "6 years ago",
+          thumbnails: {
+             id: "stRg7XmWWV4",
+             url: "https://i.ytimg.com/vi/stRg7XmWWV4/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAFGNXGfDm0KaHgUr2NGl2xMO1HAg",
+             height: 404,
+             width: 720
+          },
+          title: "Karol G, Ozuna - Hello (Official Video)",
+          views: "290942215"
+       },
+       views: "290942215"
+    },
+    {
+       id: {
+          videoId: "KZS8d0zXaUU"
+       },
+       url: "https://www.youtube.com/watch?v=KZS8d0zXaUU",
+       title: "Hello",
+       channelName: "Kes The Band",
+       description: "Provided to YouTube by AdvoKit Productions  Hello  · KES Folklore Riddim ℗ 2017 AdvoKit Productions Released on: 2017-11-08 ...",
+       duration_raw: "3:19",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=KZS8d0zXaUU",
+          duration: "3:19",
+          publishedAt: null,
+          thumbnails: {
+             id: "KZS8d0zXaUU",
+             url: "https://i.ytimg.com/vi/KZS8d0zXaUU/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBZoEDVwy7YVTcsRsvkyBEz394v3A",
+             height: 404,
+             width: 720
+          },
+          title: "Hello",
+          views: "299495"
+       },
+       views: "299495"
+    },
+    {
+       id: {
+          videoId: "eldeaIAv_wE"
+       },
+       url: "https://www.youtube.com/watch?v=eldeaIAv_wE",
+       title: "Kes - Hello (Folklore Riddim) \"2018 Soca\" [AdvoKit Productions x Julianspromos]",
+       channelName: "JulianspromosTV | Soca Music",
+       description: "Folklore is tradition. Folklore is culture. Folklore is we! Get your heart pumping with every beat of the drum reminding us that we're ...",
+       duration_raw: "3:19",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=eldeaIAv_wE",
+          duration: "3:19",
+          publishedAt: "5 years ago",
+          thumbnails: {
+             id: "eldeaIAv_wE",
+             url: "https://i.ytimg.com/vi/eldeaIAv_wE/hq720.jpg?sqp=-oaymwExCNAFEJQDSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYciBeKDUwDw==&rs=AOn4CLAXJtHPNE7WK01JO6v51rMeH6HLqA",
+             height: 404,
+             width: 720
+          },
+          title: "Kes - Hello (Folklore Riddim) \"2018 Soca\" [AdvoKit Productions x Julianspromos]",
+          views: "37517721"
+       },
+       views: "37517721"
+    },
+    {
+       id: {
+          videoId: "jK5FOyMhDOY"
+       },
+       url: "https://www.youtube.com/watch?v=jK5FOyMhDOY",
+       title: "01 - Ice Cube - Hello",
+       channelName: "O'Shea Jackson",
+       description: "War & Peace Vol. 2 (The Peace Disc) is the sixth studio album by American rapper Ice Cube, released March 21, 2000 on Priority ...",
+       duration_raw: "3:59",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=jK5FOyMhDOY",
+          duration: "3:59",
+          publishedAt: "10 years ago",
+          thumbnails: {
+             id: "jK5FOyMhDOY",
+             url: "https://i.ytimg.com/vi/jK5FOyMhDOY/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLD3yutkv2GoTTiz4P57A87oYPE_Kw",
+             height: 404,
+             width: 720
+          },
+          title: "01 - Ice Cube - Hello",
+          views: "7206218"
+       },
+       views: "7206218"
+    },
+    {
+       id: {
+          videoId: "t071Wm45qWM"
+       },
+       url: "https://www.youtube.com/watch?v=t071Wm45qWM",
+       title: "Conkarah & Rosie Delmah - Hello (Reggae Cover) [Official Video]",
+       channelName: "Ultra Records",
+       description: "Ultra Music is an American independent electronic music record label based in New York City. Ultra's current roster includes ...",
+       duration_raw: "4:23",
+       snippet: {
+          url: "https://www.youtube.com/watch?v=t071Wm45qWM",
+          duration: "4:23",
+          publishedAt: "6 years ago",
+          thumbnails: {
+             id: "t071Wm45qWM",
+             url: "https://i.ytimg.com/vi/t071Wm45qWM/hq720.jpg?sqp=-oaymwExCNAFEJQDSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYZSBZKEgwDw==&rs=AOn4CLCZP9gmUuTqtKAse2JslspXNu4reQ",
+             height: 404,
+             width: 720
+          },
+          title: "Conkarah & Rosie Delmah - Hello (Reggae Cover) [Official Video]",
+          views: "17233858"
+       },
+       views: "17233858"
+    }
+ ];
+
+export  default sampleVideos;
