0d5eee40089ca5df24afcc68680d31a290095334
fortune-max <fortmax120@gmail.com>
2023-03-09 10:22:45 +0100

    abstract VideoPlayer component from VideoDetails

---
+++ src/VideoPlayer.tsx
@@ -0,0 +1,52 @@
+import useSWR from "swr";
+import YouTube from "react-youtube";
+import { timeAgo, formatNum } from "./utils";
+import {
+    BoldText, 
+    ChannelCard, 
+    ChannelDetails, 
+    ChannelThumbnail, 
+    FlexColumn, 
+    ThinText, 
+    VideoDescription, 
+    VideoDetailContainer, 
+    VideoDetails, 
+    VideoStats, 
+    VideoTitle
+} from "./VideoPlayer.styled";
+
+const VideoPlayer = ({ videoId }: {
+    videoId: string;
+}) => {
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+    const { data } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoId}`, fetcher);
+
+    return (
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+            <VideoDetailContainer>
+                <YouTube style={{width: "640px", height: "360px"}} videoId={videoId} />
+                <VideoDetails>
+                    <VideoTitle>{data?.title}</VideoTitle>
+                    <VideoStats>
+                        <ChannelCard>
+                            <ChannelThumbnail src={"https://picsum.photos/100/100"} alt="thumbnail" />
+                            <ChannelDetails>
+                                <BoldText>{data?.owner}</BoldText>
+                                <ThinText>{formatNum(Math.round(data?.views / 123))} subscribers</ThinText>
+                            </ChannelDetails>
+                        </ChannelCard>
+                        <FlexColumn>
+                            <BoldText>{`${formatNum(Math.round(data?.views / 161))} likes`}</BoldText>
+                            <ThinText>{`${timeAgo(data?.datePublished)} ago`}</ThinText>
+                        </FlexColumn>
+                    </VideoStats>
+                    
+                    <VideoDescription>{data?.description}</VideoDescription>
+                </VideoDetails>
+            </VideoDetailContainer>
+        </div>
+    );
+};
+
+export default VideoPlayer;
